93702e40a5f606c198a69176ec4a481add2925f9
randomcoww <etherblimp@gmail.com>
2018-01-22 22:34:01 -0800

    whitespace formatting

--- go-libvirt-mon.go
+++ go-libvirt-mon.go
@@ -1,115 +1,115 @@
 package main
 
 import (
-  "flag"
-  "fmt"
-  "time"
-  "os"
+	"flag"
+	"fmt"
+	"time"
+	"os"
 	"os/signal"
-  "syscall"
-  libvirt "github.com/libvirt/libvirt-go"
+	"syscall"
+	libvirt "github.com/libvirt/libvirt-go"
 )
 
 var (
-  xmlConfig = flag.String("c", "", "XML config")
+	xmlConfig = flag.String("c", "", "XML config")
 	host = flag.String("s", "", "Server")
 
-  sigChan chan os.Signal
+	sigChan chan os.Signal
 )
 
 
 type Connection struct {
-  Conn  *libvirt.Connect
-  Host  string
+	Conn  *libvirt.Connect
+	Host  string
 }
 
 
 func NewConn(host string) (*Connection, error) {
-  c, err := libvirt.NewConnect(host)
+	c, err := libvirt.NewConnect(host)
 
-  if err != nil {
-    return nil, err
-  }
+	if err != nil {
+		return nil, err
+	}
 
-  c.SetKeepAlive(10, 2)
+	c.SetKeepAlive(10, 2)
 
-  return &Connection{
-    Conn:  c,
-    Host:  host,
-  }, nil
+	return &Connection{
+		Conn:  c,
+		Host:  host,
+	}, nil
 }
 
 func (conn *Connection) Reconnect() error {
-  c, err := libvirt.NewConnect(conn.Host)
+	c, err := libvirt.NewConnect(conn.Host)
 
-  if err != nil {
-    return err
-  }
+	if err != nil {
+		return err
+	}
 
-  conn.Conn = c
-  return nil
+	conn.Conn = c
+	return nil
 }
 
 
 func (conn *Connection) GetDomain(config string) (*libvirt.Domain, error) {
-  err := conn.Reconnect()
+	err := conn.Reconnect()
 
-  if err != nil {
-    return nil, err
-  }
+	if err != nil {
+		return nil, err
+	}
 
-  dom, err := conn.Conn.DomainCreateXML(config, libvirt.DOMAIN_NONE)
+	dom, err := conn.Conn.DomainCreateXML(config, libvirt.DOMAIN_NONE)
 
-  if err != nil {
-    libvirtErr, _ := err.(libvirt.Error)
+	if err != nil {
+		libvirtErr, _ := err.(libvirt.Error)
 
-    switch libvirtErr.Code {
-    case 9:
-      uuid := string(libvirtErr.Message[len(libvirtErr.Message)-36:])
+		switch libvirtErr.Code {
+		case 9:
+			uuid := string(libvirtErr.Message[len(libvirtErr.Message)-36:])
 
-      fmt.Println("Found UUID", uuid)
-      dom, err = conn.Conn.LookupDomainByUUIDString(uuid)
+			fmt.Println("Found UUID", uuid)
+			dom, err = conn.Conn.LookupDomainByUUIDString(uuid)
 
-      if err != nil {
-        return nil, err
-      }
+			if err != nil {
+				return nil, err
+			}
 
-    default:
-      fmt.Println("reconnect", libvirtErr.Code, libvirtErr.Domain)
-      return nil, err
-    }
-  }
+		default:
+			fmt.Println("reconnect", libvirtErr.Code, libvirtErr.Domain)
+			return nil, err
+		}
+	}
 
-  name, err := dom.GetName()
+	name, err := dom.GetName()
 
-  if err != nil {
-    return nil, err
-  }
+	if err != nil {
+		return nil, err
+	}
 
-  fmt.Println("Created domain", name)
+	fmt.Println("Created domain", name)
 
-  return dom, nil
+	return dom, nil
 }
 
 
 func main() {
-  flag.Parse()
+	flag.Parse()
 
-  fmt.Println("Connect", *host)
+	fmt.Println("Connect", *host)
 
-  conn, err := NewConn(*host)
+	conn, err := NewConn(*host)
 
-  if err != nil {
-    panic(err)
-  }
+	if err != nil {
+		panic(err)
+	}
 
-  dom, err := conn.GetDomain(*xmlConfig)
+	dom, err := conn.GetDomain(*xmlConfig)
 
-  if err != nil {
-    panic(err)
-  }
+	if err != nil {
+		panic(err)
+	}
 
-  sigChan := make(chan os.Signal, 1)
+	sigChan := make(chan os.Signal, 1)
 
 	signal.Notify(sigChan,
 		syscall.SIGHUP,
@@ -117,25 +117,25 @@ func main() {
 		syscall.SIGTERM,
 		syscall.SIGQUIT)
 
-  for {
-    select {
-    case <-sigChan:
+	for {
+		select {
+		case <-sigChan:
 
-      fmt.Println("Got kill")
+			fmt.Println("Got kill")
 
-      dom, err = conn.GetDomain(*xmlConfig)
+			dom, err = conn.GetDomain(*xmlConfig)
 
-      if err != nil {
-        panic(err)
-      }
+			if err != nil {
+				panic(err)
+			}
 
-      dom.Destroy()
-      dom.Undefine()
-      time.Sleep(5000 * time.Millisecond)
+			dom.Destroy()
+			dom.Undefine()
+			time.Sleep(5000 * time.Millisecond)
 
-      os.Exit(0)
+			os.Exit(0)
 
-    case <-time.After(100 * time.Millisecond):
-    }
-  }
+		case <-time.After(100 * time.Millisecond):
+		}
+	}
 }
