4645709caee39ac8f8966384844ed0e7f9045e67
randomcoww <etherblimp@gmail.com>
2018-01-25 01:15:17 -0800

    handling shutdown -> destroy

--- go-libvirt-mon.go
+++ go-libvirt-mon.go
@@ -15,50 +15,57 @@ var (
 	host = flag.String("s", "", "Server")
 
 	sigChan chan os.Signal
+	shutdownWait chan struct{}
 )
 
 
-type Connection struct {
-	Conn  *libvirt.Connect
-	Host  string
+type Connect struct {
+	Connect	*libvirt.Connect
+	Host		string
+}
+
+type Domain struct {
+	Domain	*libvirt.Domain
+	Config	string
 }
 
 
-func NewConn(host string) (*Connection, error) {
+func NewConn(host string) (*Connect, error) {
 	c, err := libvirt.NewConnect(host)
 
 	if err != nil {
 		return nil, err
 	}
 
-	c.SetKeepAlive(10, 2)
+	c.SetKeepAlive(5, 2)
 
-	return &Connection{
-		Conn:  c,
-		Host:  host,
+	return &Connect{
+		Connect:	c,
+		Host: 		host,
 	}, nil
 }
 
-func (conn *Connection) Reconnect() error {
+func (conn *Connect) Reconnect() error {
 	c, err := libvirt.NewConnect(conn.Host)
 
 	if err != nil {
 		return err
 	}
 
-	conn.Conn = c
+	conn.Connect = c
 	return nil
 }
 
 
-func (conn *Connection) GetDomain(config string) (*libvirt.Domain, error) {
+func (conn *Connect) NewDomain(config string) (*Domain, error) {
 	err := conn.Reconnect()
 
 	if err != nil {
 		return nil, err
 	}
 
-	dom, err := conn.Conn.DomainCreateXML(config, libvirt.DOMAIN_NONE)
+	// dom, err := conn.Connect.DomainCreateXML(config, libvirt.DOMAIN_NONE)
+	dom, err := conn.Connect.DomainDefineXML(config)
 
 	if err != nil {
 		libvirtErr, _ := err.(libvirt.Error)
@@ -67,28 +74,125 @@ func (conn *Connection) GetDomain(config string) (*libvirt.Domain, error) {
 		case 9:
 			uuid := string(libvirtErr.Message[len(libvirtErr.Message)-36:])
 
-			fmt.Println("Found UUID", uuid)
-			dom, err = conn.Conn.LookupDomainByUUIDString(uuid)
+			fmt.Println("Domain found running", uuid)
+			dom, err = conn.Connect.LookupDomainByUUIDString(uuid)
 
 			if err != nil {
 				return nil, err
 			}
 
 		default:
-			fmt.Println("reconnect", libvirtErr.Code, libvirtErr.Domain)
 			return nil, err
 		}
 	}
 
-	name, err := dom.GetName()
+	active, err := dom.IsActive()
 
 	if err != nil {
 		return nil, err
 	}
 
-	fmt.Println("Created domain", name)
+	if !active {
+		err = dom.Create()
 
-	return dom, nil
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return &Domain {
+		Domain:		dom,
+		Config:		config,
+	}, nil
+}
+
+func (dom *Domain) Monitor() {
+	signal.Notify(sigChan, syscall.SIGTERM)
+
+	for {
+		select {
+		case s := <-sigChan:
+			fmt.Println("Got signal", s)
+
+			err := dom.Domain.Shutdown()
+
+			if err != nil {
+				panic(err)
+			}
+
+			shutdownWait <-struct{}{}
+			return
+
+		case <-time.After(5000 * time.Millisecond):
+			active, err := dom.Domain.IsActive()
+
+			if err != nil {
+				panic(err)
+			}
+
+			if !active {
+				fmt.Println("VM inactive - restarting")
+
+				err = dom.Domain.Create()
+
+				if err != nil {
+					panic(err)
+				}
+			}
+		}
+	}
+}
+
+func (dom *Domain) Shutdown() {
+
+	timerDestroy := time.NewTimer(8000 * time.Millisecond)
+
+	for {
+		select {
+		case <-timerDestroy.C:
+			fmt.Println("Shutdown timed out")
+
+			err := dom.Domain.Destroy()
+
+			if err != nil {
+				libvirtErr, _ := err.(libvirt.Error)
+
+				switch libvirtErr.Code {
+        // not running
+				case 55:
+					os.Exit(0)
+
+				default:
+					panic(err)
+				}
+			}
+
+			fmt.Println("Sending destroy")
+			os.Exit(0)
+
+		case <-time.After(1000 * time.Millisecond):
+			active, err := dom.Domain.IsActive()
+
+			if err != nil {
+				libvirtErr, _ := err.(libvirt.Error)
+
+				switch libvirtErr.Code {
+        // not running
+				case 55:
+					fmt.Println("Shutdown")
+					os.Exit(0)
+
+				default:
+					panic(err)
+				}
+			}
+
+			if !active {
+				fmt.Println("Shutdown")
+				os.Exit(0)
+			}
+		}
+	}
 }
 
 
@@ -103,39 +207,23 @@ func main() {
 		panic(err)
 	}
 
-	dom, err := conn.GetDomain(*xmlConfig)
+	dom, err := conn.NewDomain(*xmlConfig)
 
 	if err != nil {
 		panic(err)
 	}
 
-	sigChan := make(chan os.Signal, 1)
+	sigChan = make(chan os.Signal, 1)
+	shutdownWait = make(chan struct{}, 1)
 
-	signal.Notify(sigChan,
-		syscall.SIGHUP,
-		syscall.SIGINT,
-		syscall.SIGTERM,
-		syscall.SIGQUIT)
+	go dom.Monitor()
 
 	for {
 		select {
-		case <-sigChan:
+		case <-shutdownWait:
+			dom.Shutdown()
 
-			fmt.Println("Got kill")
-
-			dom, err = conn.GetDomain(*xmlConfig)
-
-			if err != nil {
-				panic(err)
-			}
-
-			dom.Destroy()
-			dom.Undefine()
-			time.Sleep(5000 * time.Millisecond)
-
-			os.Exit(0)
-
-		case <-time.After(100 * time.Millisecond):
+		case <-time.After(1000 * time.Millisecond):
 		}
 	}
 }
